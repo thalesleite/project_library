1ab9b61079bff2ab6844a4dc6465985d29b313ae
thalesleite <thalesaleite@gmail.com>
2024-04-24 15:16:04 +0100

    add remove and read toggle button to the books

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const formbook = document.getElementById("formBook")
+const booksSection = document.querySelector(".books")
 const myLibrary = []
 
 formbook.addEventListener("submit", (e) => {
@@ -7,6 +8,7 @@ formbook.addEventListener("submit", (e) => {
   const author = document.getElementById("author").value
   const pages = document.getElementById("pages").value
   const read = document.getElementById("read").value === "1" ? true : false
+  console.log("read", read)
 
   const book = {
     title,
@@ -16,7 +18,6 @@ formbook.addEventListener("submit", (e) => {
   }
 
   addBookToLibrary(book)
-  console.log("submitted!!!", book)
   clear()
 })
 
@@ -34,8 +35,68 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
+Book.prototype.toggleRead = function () {
+  this.read = !this.read
+}
+
 function addBookToLibrary(data) {
   const book = new Book(data.title, data.author, data.pages, data.read)
   myLibrary.push(book)
-  console.log("myLibrary: ", myLibrary)
+  updateBooks()
+}
+
+function updateBooks() {
+  booksSection.innerHTML = ""
+
+  if (myLibrary.length > 0) {
+    myLibrary.forEach((book, index) => {
+      const div = document.createElement("div")
+      div.classList.add("book")
+
+      const pTitle = document.createElement("p")
+      pTitle.textContent = `Title: ${book.title}`
+      div.appendChild(pTitle)
+
+      const pAuthor = document.createElement("p")
+      pAuthor.textContent = `Author: ${book.author}`
+      div.appendChild(pAuthor)
+
+      const pPages = document.createElement("p")
+      pPages.textContent = `Total of Pages: ${book.pages}`
+      div.appendChild(pPages)
+
+      const btnDelete = document.createElement("button")
+      btnDelete.classList.add("btn-delete")
+      btnDelete.setAttribute("data-id", index)
+      btnDelete.textContent = "REMOVE"
+      div.appendChild(btnDelete)
+
+      const btnToggleRead = document.createElement("button")
+      btnToggleRead.classList.add("btn-read")
+      btnToggleRead.textContent = book.read ? "READ" : "NOT READ"
+      div.appendChild(btnToggleRead)
+
+      booksSection.appendChild(div)
+
+      btnDelete.addEventListener("click", () => {
+        deleteBook(btnDelete.dataset.id)
+      })
+
+      btnToggleRead.addEventListener("click", () => {
+        toogleReadBook(book)
+      })
+    })
+  } else {
+    booksSection.innerHTML = "No books in the Library"
+  }
+}
+
+function deleteBook(id) {
+  myLibrary.splice(id, 1)
+  updateBooks()
+}
+
+function toogleReadBook(book) {
+  book.toggleRead()
+  updateBooks()
 }
